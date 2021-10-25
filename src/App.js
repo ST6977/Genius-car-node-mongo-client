8daef5cc6491367ce6e36c53cc60143e650e6c84
ST6977 <sumaiyat610@gmail.com>
2021-10-25 05:49:28 -0700

    all added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Login from './Pages/Login/Login/Login';
 import Header from './Pages/Shared/Header/Header';
 import AuthProvider from './contexts/AuthProvider';
 import PrivateRoute from './Pages/Login/PrivateRoute/PrivateRoute';
+import AddServices from './Pages/AddServices/AddServices';
+import ManageServices from './Pages/ManageServices/ManageServices';
 
 
 function App() {
@@ -27,7 +29,15 @@ function App() {
             </Route>
             <PrivateRoute path="/booking/:serviceId">
               <Booking></Booking>
+
             </PrivateRoute>
+
+           <Route path="/addServices">
+             <AddServices></AddServices>
+           </Route>
+           <Route path="/manageServices">
+             <ManageServices></ManageServices>
+             </Route>
             <Route path="*">
               <NotFound></NotFound>
             </Route>
